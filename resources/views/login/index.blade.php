c6364d45967326b6d7283eeb411cc9f749f94abe
motohiro saitou <tyuuou2318@gmail.com>
2024-02-08 14:16:42 +0900

    web基礎　10章完了

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -7,16 +7,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title></title>
+
+    <link rel="stylesheet" href="/css/build/login/index.css">
 </head>
 
-<body>
-    <main>
+<body class="login">
+    <main id="login">
         <?php if ($variables["isLoginActive"]) { ?>
             <h2 class="title">ログイン状態</h2>
             <a href="/login/unregister">ログアウト</a>
-            <?php } else { ?>
+        <?php } else { ?>
             <h2 class="title">新規登録</h2>
-            <form ref="registrForm" method="post" action="/login/register">
+            <form ref="registerForm" method="post" action="/login/register">
                 @csrf
                 <div>
                     ID : <input type="text" v-model="id" name="id">
@@ -25,7 +27,7 @@
                     PW : <input type="password" v-model="password" name="password">
                 </div>
                 <div>
-                    <input type="submit" v-on:click="registerSubmit" value="送信">
+                    <button type="button" v-on:click="registerSubmit">送信</button>
                 </div>
             </form>
 
@@ -39,12 +41,12 @@
                     PW : <input type="password" v-model="password" name="password">
                 </div>
                 <div>
-                    <input type="submit" v-on:click="loginSubmit" value="送信">
+                    <button type="button" v-on:click="loginSubmit">送信</button>
                 </div>
             </form>
-            <?php } ?>
-        </main>
-        <script src="/js/build/login/index.js"></script>
+        <?php } ?>
+    </main>
+    <script src="/js/build/login/index.js"></script>
 </body>
 
 </html>
