6dda5588eb3ab8a95faf49adce1884b3fb8a2d83
motohiro saitou <tyuuou2318@gmail.com>
2024-02-14 14:41:01 +0900

    modal レイアウト修正

--- resources/views/top/index.blade.php
+++ resources/views/top/index.blade.php
@@ -9,7 +9,7 @@
         <link rel="stylesheet" href="/css/build/top/index.css">
     </head>
     <body>
-        <div class="justifiy-content-center" id="app" >
+        <div id="app" >
             <modal></modal>
         </div>
 
