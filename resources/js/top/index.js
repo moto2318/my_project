41552bdc675a97e98a76c8a6cafa2fb730b212f5
motohiro saitou <tyuuou2318@gmail.com>
2024-02-14 16:29:11 +0900

    modal　コード表記の変更

--- resources/js/top/index.js
+++ resources/js/top/index.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue';
 import Modal from './Modal.vue';
 
-const app = createApp({})
-    .component('modal', Modal)
-    .mount('#app')
+createApp({
+    components: {
+        Modal
+    }
+}).mount("#app")
